cb48b78006cfee86f01863870367610b404cd7a8
DedlySpyder <pooreboy464@gmail.com>
2016-06-04 18:40:25 -0400

    Final 0.2.0 Changes

--- Generic_Logistic_Chest_0.2.0/prototypes/entities/replacement_entities.lua
+++ Generic_Logistic_Chest_0.2.0/prototypes/entities/replacement_entities.lua
@@ -1,110 +1,21 @@
-data:extend({
+local generic_logistic_chest_passive_provider = util.table.deepcopy(data.raw["logistic-container"]["logistic-chest-passive-provider"])
+local generic_logistic_chest_active_provider = util.table.deepcopy(data.raw["logistic-container"]["logistic-chest-active-provider"])
+local generic_logistic_chest_storage = util.table.deepcopy(data.raw["logistic-container"]["logistic-chest-storage"])
+local generic_logistic_chest_requester = util.table.deepcopy(data.raw["logistic-container"]["logistic-chest-requester"])
 
-  {
-    type = "logistic-container",
-    name = "generic-logistic-chest-passive-provider",
-    icon = "__base__/graphics/icons/logistic-chest-passive-provider.png",
-    flags = {"placeable-player", "player-creation"},
-    minable = {hardness = 0.2, mining_time = 0.5, result = "generic-logistic-chest"},
-    max_health = 150,
-    corpse = "small-remnants",
-    collision_box = {{-0.35, -0.35}, {0.35, 0.35}},
-    selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
-    fast_replaceable_group = "container",
-    inventory_size = 48,
-    logistic_mode = "passive-provider",
-    open_sound = { filename = "__base__/sound/metallic-chest-open.ogg", volume=0.65 },
-    close_sound = { filename = "__base__/sound/metallic-chest-close.ogg", volume = 0.7 },
-    vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
-    picture =
-    {
-      filename = "__base__/graphics/entity/logistic-chest/logistic-chest-passive-provider.png",
-      priority = "extra-high",
-      width = 38,
-      height = 32,
-      shift = {0.1, 0}
-    },
-    circuit_wire_max_distance = 7.5,
-	order ="z"
-  },
-  {
-    type = "logistic-container",
-    name = "generic-logistic-chest-active-provider",
-    icon = "__base__/graphics/icons/logistic-chest-active-provider.png",
-    flags = {"placeable-player", "player-creation"},
-    minable = {hardness = 0.2, mining_time = 0.5, result = "generic-logistic-chest"},
-    max_health = 150,
-    corpse = "small-remnants",
-    collision_box = {{-0.35, -0.35}, {0.35, 0.35}},
-    selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
-    fast_replaceable_group = "container",
-    inventory_size = 48,
-    logistic_mode = "active-provider",
-    open_sound = { filename = "__base__/sound/metallic-chest-open.ogg", volume=0.65 },
-    close_sound = { filename = "__base__/sound/metallic-chest-close.ogg", volume = 0.7 },
-    vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
-    picture =
-    {
-      filename = "__base__/graphics/entity/logistic-chest/logistic-chest-active-provider.png",
-      priority = "extra-high",
-      width = 38,
-      height = 32,
-      shift = {0.1, 0}
-    },
-    circuit_wire_max_distance = 7.5,
-	order ="z"
-  },
-  {
-    type = "logistic-container",
-    name = "generic-logistic-chest-storage",
-    icon = "__base__/graphics/icons/logistic-chest-storage.png",
-    flags = {"placeable-player", "player-creation"},
-    minable = {hardness = 0.2, mining_time = 0.5, result = "generic-logistic-chest"},
-    max_health = 150,
-    corpse = "small-remnants",
-    collision_box = {{-0.35, -0.35}, {0.35, 0.35}},
-    selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
-    fast_replaceable_group = "container",
-    inventory_size = 48,
-    logistic_mode = "storage",
-    open_sound = { filename = "__base__/sound/metallic-chest-open.ogg", volume=0.65 },
-    close_sound = { filename = "__base__/sound/metallic-chest-close.ogg", volume = 0.7 },
-    vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
-    picture =
-    {
-      filename = "__base__/graphics/entity/logistic-chest/logistic-chest-storage.png",
-      priority = "extra-high",
-      width = 38,
-      height = 32,
-      shift = {0.1, 0}
-    },
-    circuit_wire_max_distance = 7.5,
-	order ="z"
-  },
-  {
-    type = "logistic-container",
-    name = "generic-logistic-chest-requester",
-    icon = "__base__/graphics/icons/logistic-chest-requester.png",
-    flags = {"placeable-player", "player-creation"},
-    minable = {hardness = 0.2, mining_time = 0.5, result = "generic-logistic-chest"},
-    max_health = 150,
-    corpse = "small-remnants",
-    collision_box = {{-0.35, -0.35}, {0.35, 0.35}},
-    selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
-    fast_replaceable_group = "container",
-    inventory_size = 48,
-    logistic_mode = "requester",
-    open_sound = { filename = "__base__/sound/metallic-chest-open.ogg", volume=0.65 },
-    close_sound = { filename = "__base__/sound/metallic-chest-close.ogg", volume = 0.7 },
-    picture =
-    {
-      filename = "__base__/graphics/entity/logistic-chest/logistic-chest-requester.png",
-      priority = "extra-high",
-      width = 38,
-      height = 32,
-      shift = {0.1, 0}
-    },
-    circuit_wire_max_distance = 7.5,
-	order ="z"
-  }
-})
+generic_logistic_chest_passive_provider.name = "generic-logistic-chest-passive-provider"
+generic_logistic_chest_active_provider.name = "generic-logistic-chest-active-provider"
+generic_logistic_chest_storage.name = "generic-logistic-chest-storage"
+generic_logistic_chest_requester.name = "generic-logistic-chest-requester"
+
+generic_logistic_chest_passive_provider.minable.result = "generic-logistic-chest"
+generic_logistic_chest_active_provider.minable.result = "generic-logistic-chest"
+generic_logistic_chest_storage.minable.result = "generic-logistic-chest"
+generic_logistic_chest_requester.minable.result = "generic-logistic-chest"
+
+generic_logistic_chest_passive_provider.order = "z"
+generic_logistic_chest_active_provider.order = "z"
+generic_logistic_chest_storage.order = "z"
+generic_logistic_chest_requester.order = "z"
+
+data:extend({generic_logistic_chest_passive_provider, generic_logistic_chest_active_provider, generic_logistic_chest_storage, generic_logistic_chest_requester})
